16258004dab178fb3c028723e44b07dd518b6e0c
Bwilly16 <benjamin.wilichowski@marquette.edu>
2023-01-19 17:50:57 -0600

    Commit of sum

--- Lab0/PartV.cpp
+++ Lab0/PartV.cpp
@@ -1,30 +1,52 @@
+/*Author: Ben Wilichowski
+ * Date: 1/19/2023
+ * Purpose: This program is designed to calculate the values within my txt file
+ * Professor: Dr.Cooper
+ * Class: BIEN 4290 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
- 
+#include <math.h>
+
 // Main
 int main()
 {
+//Variables
     FILE* fileopen;
     char ch;
- 
+    int numberArray[20];
+    int i;
+    int sum = 0;
+  //  double avg = 0;
+  //  double gavgPRE = 1;
+  //  double gavg = 0;
+     
     // Opening file in reading mode
     fileopen = fopen("random.txt", "r");
  
     if (NULL == fileopen) {
         printf("file can't be opened \n");
+    } 
+    else{
+	    printf("File opened successfully!\n");
     }
- 
-    printf("content of this file are \n");
+   
+//Print into an array for math purposes
+    for (i = 0; i < 20; i++)
+    {
+        fscanf(fileopen, "%d", &numberArray[i]);
+	printf("Value is: %d\n\n", numberArray[i]);
+	sum = sum + numberArray[i];
+//	avg = sum/20;
+//	gavgPRE = (gavgPRE*numberArray[i]);
+//	gavg = pow(gavgPRE,0.05);
+	
+    }
+	printf("Sum = %i\n",sum);//print out of sum
+//	printf("Average = %f\n",avg);//print out of average
+//	printf("Geometric Average = %f\n",gavg);
 
-     do {
-        ch = fgetc(fileopen);
-        printf("%c", ch);
-
-        // If it is EOF stop reading.
-    } while (ch != EOF);
-
-    // Closing the file
     fclose(fileopen);
-    return 0;
+
 }
