357b6a6168ff593aadd6031e4d34bb7cce95a192
Bwilly16 <benjamin.wilichowski@marquette.edu>
2023-01-24 14:16:16 -0600

    updated commits

---
+++ in_class/bwilichowski_basicbash.sh
@@ -0,0 +1,8 @@
+#i/bin/bash
+# Ben Wilichowski
+# 1/23/2023
+# First Bash Script in class!
+
+echo hello;
+echo there are two hidden files;
+echo the highest usb is 003;
---
+++ mini0/wilichowskib_miniproject0.hpp
@@ -0,0 +1,20 @@
+/* Author: Ben Wilichowski
+ * Date Created: 1/20/2023
+ * Purpose: The purpose of this program is to hack time
+ * */
+#ifndef VARNAME
+#define VARNAME
+
+#include <iostream>
+#include <unistd.h>
+#define Hacking
+
+
+
+
+
+
+
+
+
+#endif
