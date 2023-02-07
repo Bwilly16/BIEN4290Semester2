a2ee8592349ed4d300e542246f31d51e8da725b2
Bwilly16 <benjamin.wilichowski@marquette.edu>
2023-02-07 14:57:01 -0600

    makefile works and data was copied over

--- Lab2/wilichowskib_vector_ops.cpp
+++ Lab2/wilichowskib_vector_ops.cpp
@@ -7,17 +7,37 @@ Class: BIEN4290
 
 #include "wilichowskib_vector_ops.hpp"
 
-vectorops::myvectorops::myvectorops(){} //constructor def. 
+
+//fuction stuff!
+vectorops::myvectorops::myvectorops()//constructor def.
+{
+    
+}  
 
 
-std::vector<float> sumstuff(std::vector<float>){
-    //return 0;
+std::vector<float> sumstuff(std::vector<float> array1){
+    return array1;
 }
 
-std::vector<float> substuff(std::vector<float>, std::vector<float>){
-    //return 0;
+std::vector<float> substuff(std::vector<float> array2, std::vector<float> array3){
+    std::vector<float> totalarray;
+
+
+    for(uint i = 0; i<=array2.size(); i++){
+        totalarray.push_back(array3[i] - array2[i]);
+    }
+    return totalarray;
 }
 
-std::vector<float> multstuff(int, std::vector<float>){
-    //return 0;
+std::vector<float> multstuff(int c, std::vector<float> array4){
+    
+    std::vector<float> multarray;
+    for(uint i = 0; i<array4.size(); i++){
+        multarray.push_back(c * array4[i]);
+    }
+    return multarray;
+}
+
+int main(void){
+    return 0;
 }
