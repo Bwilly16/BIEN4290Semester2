98ea245df5f0b85f6e1dc2962b7a055211636185
Bwilly16 <benjamin.wilichowski@marquette.edu>
2023-02-15 10:50:12 -0600

    In class example of bash expantiosn

--- in_class/bash_expansions.sh
+++ in_class/bash_expansions.sh
@@ -9,9 +9,9 @@ root=$(ls / | wc -w)
 home=$(ls ~ | wc -w)
 
 #command expansion
-((app_to_ls = $root+$home))
-((app_to_ls = $(ls / | wc -w)+(ls ~ | wc -w))
+(app_to_ls = $root+$home)
+(app_to_ls = $(ls / | wc -w)+(ls ~ | wc -w))
 echo $app_to_ls
 
-captains=("Picard", "Janeway", "Kirk")
-echo ${captains[0]}
+captains={"Picard", "Janeway", "Kirk"}
+echo "My favorite captain is {Picard, Janeway, Kirk}"
